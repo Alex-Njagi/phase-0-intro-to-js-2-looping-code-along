4972a8fb97208694c00ebd364dc3dedb8901b33c
Alex Njagi <1992anjagi@gmail.com>
2023-03-22 14:19:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+// const gifts = [`Guadalupe`, `Ollie`, `Aki`]
+function writeCards(gifts) {
+    const thankYouMessages = [];
+
+    for (let index=0; index<gifts.length; index++){
+        thankYouMessages.push(`Thank you, ${gifts[index]}, for the wonderful surprise gift!`)
+        // console.log(`Thank you, ${gifts[index]}, for the wonderful surprise gift!`)
+    }
+    thankYouMessages
+    return thankYouMessages
+    // return gifts;
+}
+writeCards([`Guadalupe`, `Ollie`, `Aki`]);
+
+let count = 10;
+
+function countDown(count){
+while (count>=0 && count<=10) {
+    //count= count-1;
+    console.log(count--);
+}}
+countDown(count)
